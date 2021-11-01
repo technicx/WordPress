693b063a79a3347b86eaf28ee1ccbb936000acc9
technicx <93508425+technicx@users.noreply.github.com>
2021-11-01 12:02:53 +0530

    Update wp-config-sample.php

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -37,6 +37,8 @@ define( 'DB_CHARSET', 'utf8' );
 /** The database collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
 
+define('WP_ALLOW_REPAIR', true);
+
 /**#@+
  * Authentication unique keys and salts.
  *
